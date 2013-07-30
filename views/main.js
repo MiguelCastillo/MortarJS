91057c049251452d0b9f3b4438ec284bdebc6135
Miguel <manchagnu@gmail.com>
2013-07-29 22:56:08 -0400

    Broke up main content into its own view.  Deleted misc jquery ui folders (they were empty).

--- views/main.js
+++ views/main.js
@@ -3,11 +3,13 @@ define(function(require, exports, module) {
   var view = require("mortar/view");
   require("views/navbar");
   require("views/mainheader");
+  require("views/maincontent");
 
 
   function init(widget) {
     widget.element.find(".navbarContainer").navbar();
     widget.element.find(".mainheaderContainer").mainheader();
+    widget.element.find(".maincontentContainer").maincontent();
   }
 
 
--- views/maincontent.js
+++ views/maincontent.js
@@ -2,11 +2,18 @@ define(function(require, exports, modules) {
 
   var view = require("mortar/view");
 
-  view("mortar.main", {
+
+  function init(widget) {
+  }
+
+
+  view("doc.maincontent", {
     options: {
     },
 
     _create: function() {
+      this.element.addClass("container");
+      init(this);
     },
 
     _destroy: function() {
