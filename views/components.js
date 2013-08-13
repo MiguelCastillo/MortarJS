b9df07d08556a52feed2a8a12796820782e569eb
Miguel <manchagnu@gmail.com>
2013-08-13 19:36:43 -0400

    Set monokai as the theme for the code editor

--- views/components.js
+++ views/components.js
@@ -21,7 +21,7 @@ define(function(require, exports, module) {
 
 
     $(".jscode", widget.element).each(function( index, item ) {
-      var $target = $("<pre class='cm-s-default'>").appendTo(item);
+      var $target = $("<pre class='cm-s-monokai'>").appendTo(item);
       var $source = $("textarea", item);
       CodeMirror.runMode($source[0].value, "application/javascript", $target[0]);
     });
